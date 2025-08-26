908c9688e365064e2fc17fbe63eaeeda4358107e
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2025-08-26 19:58:50 -0300

    status 404 na parte de consulta

--- src/app.js
+++ src/app.js
@@ -32,6 +32,7 @@ app.get('/cursos', (req, res) => {
     conexao.query(sql, (error, result) => {
         if (error){
             console.log(error)
+            res.status(404).json({'error': error})
         }
         else {
             res.status(200).json(result)
